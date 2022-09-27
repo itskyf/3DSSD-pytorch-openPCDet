14aca19c8156003a87a871532e4269ecaaa409c5
Ky Anh <40734986+itskyf@users.noreply.github.com>
2022-09-27 16:24:12 +0700

    feat: recover ops

--- setup.py
+++ setup.py
@@ -76,6 +76,14 @@ if __name__ == '__main__':
                 ],
             ),
             make_cuda_ext(
+                name='roiaware_pool3d_cuda',
+                module='pcdet.ops.roiaware_pool3d',
+                sources=[
+                    'src/roiaware_pool3d.cpp',
+                    'src/roiaware_pool3d_kernel.cu',
+                ]
+            ),
+            make_cuda_ext(
                 name='roipoint_pool3d_cuda',
                 module='pcdet.ops.roipoint_pool3d',
                 sources=[
